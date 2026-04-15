5ee18a9b79bed4366b146c0a47f34437ab8ed1dd
loki <deho2k@gmail.com>
2026-04-16 00:51:04 +0300

    fix

---
+++ hosts/server/opts/packages.nix
@@ -0,0 +1,22 @@
+{pkgs,inputs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.brightnessctl
+    pkgs.tcpdump
+    pkgs.traceroute
+    inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
+  ];
+  services.upower.enable = true;
+  programs.neovim = {
+    enable = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+    defaultEditor = true;
+  };
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    noto-fonts
+    noto-fonts-color-emoji
+  ];
+}
